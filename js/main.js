ee5ea847932c1ceaa6cdb15dc7c0bfea6023c8ca
Reeves Player <106031094+reevesplayer@users.noreply.github.com>
2023-05-21 04:10:17 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const week = day * 7;
+const month = week * 4;
+const year = month * 12;
+
+let count_down = new Date('03/03/2025 00:00:00').getTime();
+let x = setInterval(() => countDown(), second);
+
+function countDown() {
+  let now = new Date(Date.now()).getTime();
+  let diff = count_down - now;
+
+  document.getElementById('years').innerText = Math.floor(diff / year);
+  document.getElementById('months').innerText = Math.floor(diff % year / month);
+  document.getElementById('weeks').innerText = Math.floor(diff % month / week);
+  document.getElementById('days').innerText = Math.floor(diff % week / day);
+  document.getElementById('hours').innerText = Math.floor(diff % day / hour);
+  document.getElementById('minutes').innerText = Math.floor(diff % hour / minute);
+  document.getElementById('seconds').innerText = Math.floor(diff % minute / second);
+}
+
+function resetCountdown() {
+  clearInterval(x);
+  let date_end = document.form_main.date_end.value;
+  count_down = new Date(`${date_end} 00:00:00`).getTime();
+  x = setInterval(() => countDown(), second);
+}
